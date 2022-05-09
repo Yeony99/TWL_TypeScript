1748b158a6f2afdb42ccec77a1511f4de4323185
Yeony99 <ny.yeony.kim@gmail.com>
2022-05-09 15:39:12 +0900

    Update JobList to get order props

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <job-list :jobs="jobs" />
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <job-list :jobs="jobs" :order="order"/>
+  </div>
 </template>
 
 <script lang="ts">
 import {defineComponent, reactive, ref, toRefs} from 'vue';
 import JobList from "./components/JobList.vue"
 import Job from "./types/Job";
+import OrderTerm from "@/types/OrderTerm";
 
 export default defineComponent({
   name: 'App',
@@ -20,11 +30,33 @@ export default defineComponent({
       { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5'},
     ])
 
-    return { jobs };
+    const order = ref<OrderTerm>('title') // default value
+
+    // 여러 타입의 변수를 컨트롤 하기 위해 OrderTerm type 생성 후 지정
+    const handleClick = (term: OrderTerm) => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order };
   },
 });
 </script>
 
 <style>
-
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 </style>
