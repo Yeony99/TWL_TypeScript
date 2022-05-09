f988a8845119917ac71ebcec66370df6294a1db0
Yeony99 <ny.yeony.kim@gmail.com>
2022-05-09 13:40:09 +0900

    Update JobList.vue layout & styles

--- frontend/src/components/JobList.vue
+++ frontend/src/components/JobList.vue
@@ -3,8 +3,11 @@
     <ul>
       <li v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
-        <div>
+        <div class="salary">
           <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>dfdfsfsfqrefds</p>
         </div>
       </li>
     </ul>
@@ -20,12 +23,43 @@ export default defineComponent({
       required: true,
       type: Array as PropType<Job[]>
     }
+  },
+  setup(props) {
+    console.log(props)
   }
 })
 </script>
 
 <style scoped>
-
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
 </style>
 
 
