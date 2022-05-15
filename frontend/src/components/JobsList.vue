ade581a0c03dc84171c2a5ac5fa467957ad32b33
recordbuffer <thal96@kyonggi.ac.kr>
2022-05-15 19:22:26 +0900

    4 types & props
    PropType

--- frontend/src/components/JobsList.vue
+++ frontend/src/components/JobsList.vue
@@ -1,15 +1,61 @@
 <template>
-    
+    <div class="job-list">
+        <ul>
+            <li v-for="job in jobs" :key="job.id">
+                <h2>{{ job.title }} in {{ job.location }}</h2>
+                <div class="salary">
+                    <p>{{ job.salary }} won</p>
+                </div>
+                <div class="description">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus natus a, placeat nemo facilis sunt similique dolore, ducimus dolorem sequi voluptas expedita cumque modi tempore, aut totam quasi consequatur veritatis?</p>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, PropType } from 'vue'
+import Job from '@/types/Job'
 
-export default defineComponent({
-
+export default defineComponent({            // Prop으로 받은 데이터의 key 알기 위해 PropType
+    props: {
+        jobs: {
+            required: true,
+            type: Array as PropType<Job[]>        
+        }
+    }
 })
 </script>
 
 <style scoped>
-
+  .job-list {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+  .job-list ul {
+    padding: 0;
+  }
+  .job-list li {
+    list-style-type: none;
+    background: white;
+    padding: 16px;
+    margin: 16px 0;
+    border-radius: 4px;
+  }
+  .job-list h2 {
+    margin: 0 0 10px;
+    text-transform: capitalize;
+  }
+  .salary {
+    display: flex;
+  }
+  .salary img {
+    width: 30px;
+  }
+  .salary p {
+    color: #17bf66;
+    font-weight: bold;
+    margin: 10px 4px;
+  }
 </style>
