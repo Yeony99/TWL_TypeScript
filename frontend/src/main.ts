899c28aec53ebc0e92d4ee688ee8268c4a32b752
Yeony99 <ny.yeony.kim@gmail.com>
2022-05-18 11:32:20 +0900

    Add router/index.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/global.css'
+import router from "../src/router";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
